33ab003efce4e30132f3605a7a4489d744f75af0
Thaysa Borges <borges.thaysa@gmail.com>
2024-04-01 16:11:54 -0300

    Cria o HTML através da JS que irá renderizar os filmes

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,17 @@
 const App = () => {
-  const el = document.createElement('div');
 
-  el.className = 'App';
-  el.textContent = 'Hola mundo!';
+  const movieEl = document.createElement('div');
+  movieEl.className = "movieEl";
+  const movieInfo = document.createElement('div');
+  movieInfo.className = 'movieInfo';
+  const movieOverview = document.createElement('div');
+  movieOverview.className = 'movieOverView';
 
-  return el;
+  movieEl.appendChild(movieInfo);
+  movieEl.appendChild(movieOverview);
+
+  return movieEl;
 };
+
 
 export default App;
