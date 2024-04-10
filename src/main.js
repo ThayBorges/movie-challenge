985e3702c2b86a24fa6a8235be09567a83597f3d
Thaysa Borges <borges.thaysa@gmail.com>
2024-04-10 15:00:06 -0300

    retira umas linhas que estavam se repetindo.

--- src/main.js
+++ src/main.js
@@ -1,22 +1,10 @@
-import { getMovies, createMovieElement } from './components/App.js';
+import App from './components/App.js';
 
-const API_KEY = 'api_key=8da53c7d14f03ceb87ea073e21c6bc1f';
-const BASE_URL = 'https://api.themoviedb.org/3/';
-const API_URL = BASE_URL + 'discover/movie?sort_by=popularity.desc&' + API_KEY;
-const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+
 
 document.addEventListener('DOMContentLoaded', () => {
   const main = document.querySelector('main');
+  main.appendChild(App());
 
-  getMovies(API_URL)
-    .then((data) => {
-      main.innerHTML = '';
-      data.results.forEach((movie) => {
-        const movieEl = createMovieElement(movie);
-        main.appendChild(movieEl);
-      });
-    })
-    .catch((error) => {
-      console.error('Error fetching movies:', error);
-    });
+  
 });
