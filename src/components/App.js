4388b3433260c59fe6e03096dccd8b675205ad59
Thaysa Borges <borges.thaysa@gmail.com>
2024-04-10 14:59:14 -0300

    Cria função que faz uma caixa de pesquisa por nome do filme

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,29 @@
+import { getMovies , searchMovies } from "../lib/tmdb.js";
+
 const API_KEY = 'api_key=8da53c7d14f03ceb87ea073e21c6bc1f';
 const BASE_URL = 'https://api.themoviedb.org/3/';
-const API_URL = BASE_URL + 'discover/movie?sort_by=popularity.desc&' + API_KEY;
+const API_URL = BASE_URL + 'discover/movie?language=pt-BR&include_adult=false&sort_by=popularity.desc&' + API_KEY;
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+const searchURL = BASE_URL + '/search/movie?' + API_KEY;
 
-function getMovies(url) {
-  return fetch(url).then((res) => res.json());
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+
+const App = () => {
+  const container = document.createElement('div');
+  getMovies()
+    .then((data) => {
+      data.results.forEach((movie) => {
+        const movieEl = createMovieElement(movie);
+        container.appendChild(movieEl);
+      });
+    })
+    .catch((error) => {
+      console.error('Error fetching movies:', error);
+    });
+    return container;
 }
+
 
 function createMovieElement({ title, poster_path, vote_average, overview, id }) {
   const movieEl = document.createElement('div');
@@ -38,4 +56,28 @@ function getColor(vote) {
   }
 }
 
-export { getMovies, createMovieElement };
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const searchTerm = search.value;
+
+  if(searchTerm) {
+      searchMovies('&query=' + searchTerm)
+      .then((data) => {
+        main.innerHTML = '';
+        data.results.forEach((movie) => {
+          const movieEl = createMovieElement(movie);
+          main.appendChild(movieEl);
+        });
+      })
+      .catch((error) => {
+        console.error('Error fetching movies:', error);
+      });
+      
+  }else{
+    getMovies(API_URL);
+  }
+})
+
+
+export default App;
