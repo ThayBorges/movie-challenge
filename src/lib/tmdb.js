c8c7e5e1f72e4b1b76b41718f9eb56d8bde72c22
Thaysa Borges <borges.thaysa@gmail.com>
2024-04-10 14:57:58 -0300

    Cria um arquivo responsavel por todas as chamas vinculadas ao TMDB

---
+++ src/lib/tmdb.js
@@ -0,0 +1,19 @@
+const API_KEY = 'api_key=8da53c7d14f03ceb87ea073e21c6bc1f';
+const BASE_URL = 'https://api.themoviedb.org/3/';
+const DISCOVER_URL = BASE_URL + 'discover/movie?language=pt-BR&include_adult=false&sort_by=popularity.desc&' + API_KEY;
+const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+const searchURL = BASE_URL + 'search/movie?language=pt-BR&include_adult=false&' + API_KEY;
+
+
+
+export const getMovies = (query='') => {
+   const url=DISCOVER_URL + '&' + query
+    return fetch(url).then((res) => res.json());
+}
+
+export const searchMovies = (query='') => {
+    const url=searchURL + '&' + query
+    console.log(url);
+     return fetch(url).then((res) => res.json());
+}
+
