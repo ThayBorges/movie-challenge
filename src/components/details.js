a5e659f064cda4364ec04fdedf8b691e0d2aa5e9
Thaysa Borges <borges.thaysa@gmail.com>
2024-04-25 08:52:26 -0300

    Acrescenta página de detalhes e as funções a ela relacionadas

--- src/components/details.js
+++ src/components/details.js
@@ -1,4 +1,5 @@
 import { getDetails , IMG_URL } from "../lib/tmdb.js";
+import { getColor } from "../lib/color.js";
 
 const Details = (id) => {
     const container = document.createElement('div');
@@ -19,13 +20,13 @@ const Details = (id) => {
     const container = document.createElement('div');
 
     const details = `
-    <a href="/">home</a>
+    <a class="buttonDetails" href="/">VOLTAR</a>
     <img src="${poster_path ? IMG_URL + poster_path : 'http://via.placeholder.com/1080x1580'}" alt="${title}">
-    <div class="movieInfo">
+    <div class="movieInfoDetails">
         <h3>${title}</h3>
         <span class="${getColor(vote_average)}">${vote_average}</span>
     </div>
-    <div class="overview">
+    <div class="overviewDetails">
         <h3>Overview</h3>
         ${overview}
         <br/> 
@@ -34,15 +35,5 @@ const Details = (id) => {
     container.innerHTML = details;
     return container;
 }
-
-function getColor(vote) {
-    if (vote >= 8) {  
-      return 'green';
-    } else if (vote >= 5) {
-      return 'orange';
-    } else {
-      return 'red';
-    }
-  }
 
 export default Details;
